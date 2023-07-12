edc534a5ee2cbe5229b5084c9d3477454ee37342
badreaz <baz.23.2019@gmail.com>
2023-07-12 17:33:00 +0400

    create a child process for execve

--- shell.c
+++ shell.c
@@ -10,18 +10,26 @@ int main(void)
 	int eof, i;
 	size_t n = 1024;;
 	char *buffer, *ptr;
+	node_t *list;
+	pid_t pid;
 
 	buffer = malloc(n);
 	while (1)
 	{
-		eof = getline(&buffer, &n, stdin);
-		if (eof == EOF)
+		eof = getline(&buffer, &n, stdin); /* read the line from stdin*/
+		if (eof == EOF)  /*check the end of a file*/
 			exit(EXIT_SUCCESS);
-		ptr = strtok(buffer, " \n");
-		while (ptr)
+		list = NULL;
+		ptr = strtok(buffer, " \n"); /*tokenize when there is a space or new line */
+		while (ptr) /*enter until the line finish*/
 		{
+			addnode(list, ptr);
 			ptr = strtok(NULL, " \n");
 		}
+		pid = fork(); /* create a child process*/
+		if (pid == 0)
+			execve(list->str, list, environ);
+		wait();
 	}
 	return (0);
 }
