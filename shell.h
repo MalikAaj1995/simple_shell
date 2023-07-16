8b9529be6e09755b779e5036e88e53be737ae9d1
badreaz <baz.23.2019@gmail.com>
2023-07-16 17:16:17 +0400

    edit execute prototype

--- shell.h
+++ shell.h
@@ -33,7 +33,7 @@ char *_strcat(char *dest, char *src);
 int _strlen(char *s);
 int _strcmp(char *s1, char *s2);
 void freelist(char **list);
-int execute(node_t *mynode);
+int execute(node_t *list);
 int _strncmp(char *s1, char *s2, int len);
 int operators(char *line);
 node_t *tokenize(char *line);
