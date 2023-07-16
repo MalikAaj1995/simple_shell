fd6364a1ddb712767938bf0dccc7ecb969687117
badreaz <baz.23.2019@gmail.com>
2023-07-16 15:54:31 +0400

    create tokenize.c

---
+++ tokenize.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * tokenize - tokenize a line using strtok
+ * @line: command line.
+ *
+ * Return: tokenized linked list.*
+ */
+node_t *tokenize(char *line)
+{
+	node_t *mynode;
+	char *ptr;
+
+	ptr = strtok(line, " \n");
+	while (ptr)
+	{
+		addnode(&mynode, ptr);
+		ptr = strtok(NULL, " \n");
+	}
+	return (mynode);
+}
